72abcc326d771d8d679320d8ef7d6bf4560da033
David Denton <denton.david@gmail.com>
2022-12-11 14:01:49 +0000

    remove deprecations, remove dust module

--- build.gradle.kts
+++ build.gradle.kts
@@ -244,7 +244,7 @@ fun Node.childrenCalled(wanted: String) = children()
 tasks.named<KotlinCompile>("compileTestKotlin") {
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs += listOf("-Xjvm-default=all", "--enable-preview")
+        freeCompilerArgs += listOf("-Xjvm-default=all")
     }
 }
 
